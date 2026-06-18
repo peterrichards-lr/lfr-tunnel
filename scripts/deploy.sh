e9d352ca6c2af44cd0949fb80ddfec5e3203f090
Peter Richards <admin@lfr-demo.se>
2026-06-18 19:16:07 +0100

    deploy: update scripts/deploy.sh to automatically upload Properties and HTML templates to VPS

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -35,6 +35,10 @@ scp $SSH_KEY -r resources/server/error_pages $VPS_USER@$VPS_IP:/home/$VPS_USER/
 echo "Uploading static assets to VPS..."
 scp $SSH_KEY -r pkg/server/static $VPS_USER@$VPS_IP:/home/$VPS_USER/
 
+echo "Uploading properties translations and email templates to VPS..."
+scp $SSH_KEY -r pkg/server/i18n $VPS_USER@$VPS_IP:/home/$VPS_USER/
+scp $SSH_KEY -r pkg/server/templates $VPS_USER@$VPS_IP:/home/$VPS_USER/
+
 echo "Executing remote deployment commands..."
 ssh $SSH_KEY $VPS_USER@$VPS_IP << REMOTE_SSH
     sudo mv /home/$VPS_USER/lfr-tunneld /usr/local/bin/lfr-tunneld
@@ -43,6 +47,16 @@ ssh $SSH_KEY $VPS_USER@$VPS_IP << REMOTE_SSH
     sudo cp -r /home/$VPS_USER/error_pages/* /var/www/lfr-tunnel/error_pages/
     sudo mkdir -p /var/www/lfr-tunnel/static
     sudo cp -r /home/$VPS_USER/static/* /var/www/lfr-tunnel/static/
+    
+    # Copy Properties and Email Templates to the secure /etc/lfr-tunneld/ path
+    sudo mkdir -p /etc/lfr-tunneld/i18n
+    sudo cp -r /home/$VPS_USER/i18n/*.properties /etc/lfr-tunneld/i18n/
+    sudo mkdir -p /etc/lfr-tunneld/templates
+    sudo cp -r /home/$VPS_USER/templates/* /etc/lfr-tunneld/templates/
+    
+    # Clean up temporary home files
+    rm -rf /home/$VPS_USER/error_pages /home/$VPS_USER/static /home/$VPS_USER/i18n /home/$VPS_USER/templates
+    
     sudo systemctl restart lfr-tunneld
 REMOTE_SSH
 

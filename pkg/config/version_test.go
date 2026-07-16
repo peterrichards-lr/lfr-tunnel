a54ac3f325069a990dfdd95813ab6f43bf83fe25
Peter Richards <admin@lfr-demo.se>
2026-07-16 11:36:18 +0100

    test: add version synchronization test for release workflow safety

---
+++ pkg/config/version_test.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestVersionSync(t *testing.T) {
+	// Path to whats-new.json relative to pkg/config
+	path := filepath.Join("..", "server", "static", "whats-new.json")
+	
+	data, err := os.ReadFile(path)
+	if err != nil {
+		t.Fatalf("Failed to read %s: %v", path, err)
+	}
+
+	var whatsNew struct {
+		Version string `json:"version"`
+	}
+
+	if err := json.Unmarshal(data, &whatsNew); err != nil {
+		t.Fatalf("Failed to parse JSON from %s: %v", path, err)
+	}
+
+	if Version != whatsNew.Version {
+		t.Errorf("Version mismatch! config.Version = %q, but whats-new.json = %q. These must be kept in sync.", Version, whatsNew.Version)
+	}
+}

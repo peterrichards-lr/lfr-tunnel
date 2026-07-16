b364bb45ec5e8083e4ac0c6652b070daeb0ac282
Peter Richards <admin@lfr-demo.se>
2026-07-16 10:46:54 +0100

    chore: release v1.37.1

--- pkg/config/version.go
+++ pkg/config/version.go
@@ -1,4 +1,4 @@
 package config
 
-// Version represents the current version of the lfr-tunnel client and server
-var Version = "v1.37.0"
+// Version represents the current build version of the application
+var Version = "v1.37.1"

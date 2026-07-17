43c18545f5da2c1f889cc0cd35b99f3be96877cd
Peter Richards <80279709+peterrichards-lr@users.noreply.github.com>
2026-07-17 15:16:04 +0100

    chore: add React UI linting and building to pre-commit and build scripts (#622)
    Co-authored-by: Peter Richards <admin@lfr-demo.se>

--- scripts/build-ui.sh
+++ scripts/build-ui.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ui
 if command -v pnpm &> /dev/null; then
-  pnpm install && pnpm run build
+  pnpm install && pnpm run lint && pnpm run build
 elif command -v npm &> /dev/null; then
-  npm install && npm run build
+  npm install && npm run lint && npm run build
 else
   echo "Error: Neither pnpm nor npm is installed."
   exit 1

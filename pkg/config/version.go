a17ac896e69efa830d365eca690c9f9a93bfe766
Peter Richards <80279709+peterrichards-lr@users.noreply.github.com>
2026-09-20 22:26:46 +0100

    chore: bump version to v1.48.41

--- pkg/config/version.go
+++ pkg/config/version.go
@@ -7,7 +7,7 @@ import (
 
 // Version is the current version of lfr-tunnel
 // This is overridden by ldflags during the build process
-var Version = "v1.48.40"
+var Version = "v1.48.41"
 
 // CompareVersions returns -1 if v1 < v2, 0 if v1 == v2, 1 if v1 > v2.
 //
